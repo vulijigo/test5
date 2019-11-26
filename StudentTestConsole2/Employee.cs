6cd1a72a28cdba5651d4b0153c899e7364bfb2a8
Brett Caudle <brettcaudle711@gmail.com>
2019-11-26 11:44:25 -0800

    Add project files.

---
+++ StudentTestConsole2/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+public class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Name { get; set; }
+
+    public Person(string firstName, string lastName)
+    {
+        Console.WriteLine(firstName + " " + lastName);
+    }
+    public Person(string firstName) : this("Brett", "Caudle")
+    {
+        Console.WriteLine(firstName);
+    }
+}
---
+++ StudentTestConsole2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _162_Const
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string programName = "The Program";
+
+            var theList = new Dictionary<string, string>();
+        }
+    }
+    
+}
